019118be7e9fa0b375a4b48a9df6c36b89f2d12b
Pietraszko <michal.pietraszko@nokia.com>
2019-09-01 00:26:12 +0200

    Added logging system

--- Luna/src/Luna.h
+++ Luna/src/Luna.h
@@ -3,6 +3,8 @@
 // For use by Luna applications
 
 #include "Luna/Application.h"
+#include "Luna/Log.h"
+
 
 // ---Entry Point----------------
 #include "Luna/EntryPoint.h"
--- Luna/src/Luna/EntryPoint.h
+++ Luna/src/Luna/EntryPoint.h
@@ -1,13 +1,16 @@
 #pragma once
 
 #ifdef LN_PLATFORM_WINDOWS
-#include <iostream>
 
 extern Luna::Application* Luna::createApplication();
 
 int main(int argc, char** argv)
 {
-	std::cout << "dupa" << std::endl;
+	Luna::Log::init();
+	LN_CORE_WARN("Initialized log!");
+	int a = 5;
+	LN_INFO("Hello! Var={0}", a);
+
 	auto app = Luna::createApplication();
 	app->run();
 	delete app;
